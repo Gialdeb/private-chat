373429a7ce1c35ac51a729a0534a456969e4774b
Giuseppe Alessandro De Blasio <alexsi@hotmail.it>
2018-07-22 23:22:11 +0200

    Creato Factory User"

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(\App\User::class)->create([
+            'email' => 'alexsi@hotmail.it',
+            'name' => 'Giuseppe'
+        ]);
     }
 }
