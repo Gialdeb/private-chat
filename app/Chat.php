2ff5725fd381c44809dbd99f1f5ad5dd54802aef
Giuseppe Alessandro De Blasio <alexsi@hotmail.it>
2018-07-23 09:46:05 +0200

    Aggiunti Model, Resource e definite le relazioni

---
+++ app/Chat.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Chat extends Model
+{
+    public function message()
+    {
+        return $this->belongsTo(Message::class);
+    }
+}
